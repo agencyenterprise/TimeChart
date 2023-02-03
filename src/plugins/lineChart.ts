a5cb3daa058859c3ec6bbde440c717d151fbb940
Antonio Eudes <eudes@ae.studio>
2023-02-03 11:02:32 -0300

    minor cleanup

--- src/plugins/lineChart.ts
+++ src/plugins/lineChart.ts
@@ -6,7 +6,7 @@ import { TimeChartPlugin } from '.';
 import { LinkedWebGLProgram, throwIfFalsy } from './webGLUtils';
 import { DataPointsBuffer } from "../core/dataPointsBuffer";
 
-const BUFFER_TEXTURE_WIDTH = 540;
+const BUFFER_TEXTURE_WIDTH = 540; // better keep as a multiple of 6
 const BUFFER_TEXTURE_HEIGHT = 360;
 const BUFFER_POINT_CAPACITY = BUFFER_TEXTURE_WIDTH * BUFFER_TEXTURE_HEIGHT;
 const BUFFER_INTERVAL_CAPACITY = BUFFER_POINT_CAPACITY - 2;
@@ -226,17 +226,13 @@ class SeriesSegmentVertexArray {
                 deriveDp(dp, 0.5, -0.5),
             ])
         const numVertices = 6;
-        console.log('bpos=', bufferPos, 'start=', start, 'n=', start)
-
-        const bufferPosAux = bufferPos > 1 ? (bufferPos - 1) * 6 + 1 : 1
+        const bufferPosAux = bufferPos > 1 ? (bufferPos - 1) * numVertices + 1 : 1
         const startAux = bufferPosAux - 1
-        const nAux = n * 6
-        // const bufferPosAux = bufferPos
-        // const startAux = start
-        // const nAux = n
+        const nAux = n * numVertices
+
         let rowStart = Math.floor((Math.max(0, bufferPosAux - startAux + nAux)) / BUFFER_TEXTURE_WIDTH);
         let rowEnd = Math.ceil((bufferPosAux + nAux - 1) / BUFFER_TEXTURE_WIDTH);
-        // console.log('*11* texWidth=', BUFFER_TEXTURE_WIDTH, 'bposAux=', bufferPosAux, ', startAux=', startAux, 'nAux=', nAux, 'rowStart=', rowStart, 'rowEnd=', rowEnd)
+
         // Ensure we have some padding at both ends of data.
         if (rowStart > 0 && start === 0 && bufferPos === rowStart * BUFFER_TEXTURE_WIDTH)
             rowStart--;
@@ -246,7 +242,7 @@ class SeriesSegmentVertexArray {
 
         const buffer = new Float32Array((rowEnd - rowStart) * BUFFER_TEXTURE_WIDTH * BUFFER_NUM_FIELDS);
         for (let r = rowStart; r < rowEnd; r++) {
-            for (let c = 0; c < BUFFER_TEXTURE_WIDTH * 6; c++) {
+            for (let c = 0; c < BUFFER_TEXTURE_WIDTH * numVertices; c++) {
                 const p = r * BUFFER_TEXTURE_WIDTH + c + 1;
                 const i = Math.max(Math.min(start + p - bufferPos, dps.length - 1), 0);
                 const dp = dps[i];
